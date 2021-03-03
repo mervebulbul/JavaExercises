e239d2ab0bf90b89c41a580d715b7775f7e35658
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-03 21:21:02 +0300

    Add files via upload

---
+++ Exercise3.java
@@ -0,0 +1,14 @@
+package exercise3;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// Print a table
+		System.out.println("i  i^2  i^3");
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(i + "    " + (i * i) + "   " + (i * i * i) + " ");
+
+		}
+	}
+
+}
