d696c9b10856dd271c36efa6669a7553d929b769
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-03 21:19:26 +0300

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,17 @@
+package exercise1;
+
+public class exercise1 {
+
+	public static void main(String[] args) {
+		// Write a program that displays Welcome to Java five times.
+		print();
+		print();
+		print();
+		print();
+		print();
+	}
+
+	public static void print() {
+		System.out.println("Welcome to Java");
+	}
+}
