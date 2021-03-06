57affca10ee75aaff7ff188d96a8d3ce330aad97
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-06 12:42:30 +0300

    Add files via upload

---
+++ Exercise6.java
@@ -0,0 +1,14 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		// TODO (Compute expressions)
+		double i;
+		i = ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5);
+		System.out.println("9.5 x 4.5 - 2.5 x 3");
+		System.out.println("____________________ = " + i);
+		System.out.println("     45.5 - 3.5");
+	}
+
+}
