b26bbce61c60ec0bc1dbfc321b4ab7e3c02a9ddb
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-06 12:39:23 +0300

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,15 @@
+package exercise4;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		System.out.print("           ");
+		System.out.println("299");
+		System.out.println("+          800");
+		System.out.println("---------------");
+		System.out.print("          ");
+		System.out.println(299 + 800);
+
+	}
+
+}
