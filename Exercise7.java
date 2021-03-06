4cc7cf48844a2bfb0e6e7f2a6e21c23ac7e08763
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-06 12:43:04 +0300

    Add files via upload

---
+++ Exercise7.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		// TODO Summation of a series
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i; // sum=sum+i
+		}
+		System.out.println("1+2+3+4+5+6+7+8+9+10= " + sum);
+	}
+
+}
