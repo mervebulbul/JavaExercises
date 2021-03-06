e3430a42ed3dbc6b7535cdfe639c0267fc6fe71d
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-06 12:42:06 +0300

    Add files via upload

---
+++ Exercise5.java
@@ -0,0 +1,14 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Name\tHeight\tGender");
+		System.out.println("---------------------");
+		System.out.println("John\t5\'7\"\tF");
+		System.out.println("Maria\t5\'4\"\tM");
+		System.out.println("Anna\t6\'2\"\tF");
+
+	}
+
+}
